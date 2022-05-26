a21c27e469403683443c1738ec9a4351af03f9c1
TJ <michael.btj@hotmail.com>
2022-05-26 13:32:08 -0400

    create testing

--- test/affirmation.test.js
+++ test/affirmation.test.js
@@ -1,1 +1,11 @@
-import { getAffirmation } from '../affirmation.js';
+import getAffirmation from '../affirmations.js';
+import { assert } from 'chai';
+
+it("returns a string", () => {
+
+  getAffirmation()
+    .then(res => {
+      console.log(res);
+      assert.typeOf(res, string);
+    })
+});
